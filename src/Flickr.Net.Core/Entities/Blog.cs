09581875b7b60d45ced281803f0145c059a40b90
Dominik <50596466+LineOfC0d3@users.noreply.github.com>
2023-06-07 07:26:22 +0200

    Update Blog.cs
    Fixed docs formatting

--- src/Flickr.Net.Core/Entities/Blog.cs
+++ src/Flickr.Net.Core/Entities/Blog.cs
@@ -6,9 +6,9 @@
 public sealed class Blog : IFlickrParsable
 {
     /// <summary>
-    /// The ID Flickr has assigned to the blog. Use this to post to the blog using <see
-    /// cref="Flickr.BlogsPostPhoto(string, string, string, string)"/> or <see
-    /// cref="Flickr.BlogsPostPhoto(string, string, string, string, string)"/>.
+    /// The ID Flickr has assigned to the blog. Use this to post to the blog using 
+    /// <see cref="Flickr.BlogsPostPhoto(string, string, string, string)"/> or
+    /// <see cref="Flickr.BlogsPostPhoto(string, string, string, string, string)"/>.
     /// </summary>
     public string BlogId { get; set; }
 
