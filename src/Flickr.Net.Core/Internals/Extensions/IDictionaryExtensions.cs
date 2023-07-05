54a33d0a0e652b0993b2c61e8630445864b19ff2
Jan <jan.schloots@bluewin.ch>
2023-07-05 20:29:06 +0200

    update

---
+++ src/Flickr.Net.Core/Internals/Extensions/IDictionaryExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace Flickr.Net.Core.Internals.Extensions;
+
+/// <summary>
+/// </summary>
+public static class IDictionaryExtensions
+{
+    /// <summary>
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TTargetValue"></typeparam>
+    /// <typeparam name="TSourceValue"></typeparam>
+    /// <param name="pairs"></param>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    /// <param name="predicate"></param>
+    /// <param name="func"></param>
+    public static void AppendIf<TKey, TTargetValue, TSourceValue>(this IDictionary<TKey, TTargetValue> pairs, TKey key, TSourceValue value, Predicate<TSourceValue> predicate, Func<TSourceValue, TTargetValue> func)
+    {
+        if (predicate(value))
+        {
+            pairs.Add(key, func(value));
+        }
+    }
+
+    /// <summary>
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TSourceValue"></typeparam>
+    /// <param name="pairs"></param>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    /// <param name="predicate"></param>
+    public static void AppendIf<TKey, TSourceValue>(this IDictionary<TKey, TSourceValue> pairs, TKey key, TSourceValue value, Predicate<TSourceValue> predicate)
+    {
+        pairs.AppendIf(key, value, predicate, x => x);
+    }
+}
--- src/Flickr.Net.Core/SearchOptions/PartialSearchOptions.cs
+++ src/Flickr.Net.Core/SearchOptions/PartialSearchOptions.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Linq;
 using Flickr.Net.Core.Internals.Extensions;
 
 namespace Flickr.Net.Core;
@@ -118,49 +119,22 @@ public record PartialSearchOptions
     /// </param>
     internal void AddToDictionary(IDictionary<string, string> parameters)
     {
-        if (MinUploadDate != DateTime.MinValue)
-        {
-            parameters.Add("min_uploaded_date", MinUploadDate.ToUnixTimestamp());
-        }
+        parameters.AppendIf("min_uploaded_date", MinUploadDate, x => x != DateTime.MinValue, x => x.ToUnixTimestamp());
 
-        if (MaxUploadDate != DateTime.MinValue)
-        {
-            parameters.Add("max_uploaded_date", MaxUploadDate.ToUnixTimestamp());
-        }
+        parameters.AppendIf("max_uploaded_date", MaxUploadDate, x => x != DateTime.MinValue, x => x.ToUnixTimestamp());
 
-        if (MinTakenDate != DateTime.MinValue)
-        {
-            parameters.Add("min_taken_date", MinTakenDate.ToMySql());
-        }
+        parameters.AppendIf("min_taken_date", MinTakenDate, x => x != DateTime.MinValue, x => x.ToMySql());
 
-        if (MaxTakenDate != DateTime.MinValue)
-        {
-            parameters.Add("max_taken_date", MaxTakenDate.ToMySql());
-        }
+        parameters.AppendIf("max_taken_date", MaxTakenDate, x => x != DateTime.MinValue, x => x.ToMySql());
 
-        if (Extras != PhotoSearchExtras.None)
-        {
-            parameters.Add("extras", Extras.ToFlickrString());
-        }
+        parameters.AppendIf("extras", Extras, x => x != PhotoSearchExtras.None, x => x.ToFlickrString());
 
-        if (SortOrder != PhotoSearchSortOrder.None)
-        {
-            parameters.Add("sort", SortOrder.ToFlickrString());
-        }
+        parameters.AppendIf("sort", SortOrder, x => x != PhotoSearchSortOrder.None, x => x.ToFlickrString());
 
-        if (PerPage > 0)
-        {
-            parameters.Add("per_page", PerPage.ToString(System.Globalization.NumberFormatInfo.InvariantInfo));
-        }
+        parameters.AppendIf("per_page", PerPage, x => x > 0, x => x.ToString(System.Globalization.NumberFormatInfo.InvariantInfo));
 
-        if (Page > 0)
-        {
-            parameters.Add("page", Page.ToString(System.Globalization.NumberFormatInfo.InvariantInfo));
-        }
+        parameters.AppendIf("page", Page, x => x > 0, x => x.ToString(System.Globalization.NumberFormatInfo.InvariantInfo));
 
-        if (PrivacyFilter != PrivacyFilter.None)
-        {
-            parameters.Add("privacy_filter", PrivacyFilter.ToString("d"));
-        }
+        parameters.AppendIf("privacy_filter", PrivacyFilter, x => x != PrivacyFilter.None, x => x.ToString("d"));
     }
 }
