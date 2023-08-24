98e429f38e41aad3192ec98c020f70bdf323e1dd
Dominik <d.dede2701@gmail.com>
2023-08-24 16:27:50 +0200

    FlickrConvert: changed to System.Text.JSON

--- src/Flickr.Net.Core/Internals/FlickrConvert.cs
+++ src/Flickr.Net.Core/Internals/FlickrConvert.cs
@@ -1,4 +1,7 @@
-﻿using System.Xml.Linq;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+using System.Xml.Linq;
+using Flickr.Net.Core.Internals.Attributes;
 using Flickr.Net.Core.Internals.ContractResolver;
 using Flickr.Net.Core.Internals.JsonConverters;
 
@@ -6,9 +9,57 @@ namespace Flickr.Net.Core.Internals;
 
 public static class FlickrConvert
 {
-    public static T DeserializeObject<T>(JsonTextReader jsonTextReader)
+    public static T DeserializeObject<T>(Utf8JsonReader jsonTextReader)
     {
-        return Instance.Deserialize<T>(jsonTextReader);
+        var options = new JsonSerializerOptions()
+        {
+            TypeInfoResolver = DefaultJsonTypeInfoResolver.WithAddedModifier(static typeInfo =>
+            {
+                JsonTypeInfo typeInfo2 = new JsonTypeInfo(); //TOOD: delete when extension method is recognized; then change all references tp typeInfo
+
+                foreach (JsonPropertyInfo property in typeInfo.Properties)
+                {
+                    var attributes = property.AttributeProvider.GetCustomAttributes(typeof(JsonPropertyGenericTypeNameAttribute), false);
+                    if (attributes.Length != 0 && attributes is JsonPropertyGenericTypeNameAttribute[] jsonAttributes)
+                    {
+                        if(jsonAttributes.Length > 1)
+                        {
+                            //TODO: what to do then?
+                            throw new InvalidOperationException($"Property can't have more than one {typeof(JsonPropertyGenericTypeNameAttribute)}");
+                        }
+                        JsonPropertyGenericTypeNameAttribute attr = jsonAttributes[0];
+                        Type type = typeInfo2.Type; //TODO: does this get the correct type? 
+                        if (!type.IsGenericType)
+                        {
+                            throw new InvalidOperationException($"{type} is not a generic type");
+                        }
+                        if (type.IsGenericTypeDefinition)
+                        {
+                            throw new InvalidOperationException($"{type} is a generic type definition, it must be a constructed generic type");
+                        }
+
+                        Type[] typeArgs = type.GetGenericArguments();
+                        if(attr.TypeParameterPosition >= typeArgs.Length)
+                        {
+                            throw new ArgumentException($"Can't get type argument at position {attr.TypeParameterPosition}; {type} has only {typeArgs.Length} type arguments");
+                        }
+                        if (typeArgs[attr.TypeParameterPosition].IsDefined(typeof(FlickrJsonPropertyNameAttribute), true))
+                        {
+                            property.Name = typeArgs[attr.TypeParameterPosition].GetCustomAttribute<FlickrJsonPropertyNameAttribute>().Name;
+                        }
+                        else
+                        {
+                            property.Name = typeArgs[attr.TypeParameterPosition].Name.ToLower();
+                        }
+                    }
+                }
+            })
+        };
+
+        options.Converters.Add(BoolConverter.Instance);
+        options.Converters.Add(TimestampToDateTimeConverter.Instance);
+
+        return System.Text.Json.JsonSerializer.Deserialize<T>(ref jsonTextReader, options);
     }
 
     public static string XmlToJson(string xml)
@@ -16,14 +67,4 @@ public static class FlickrConvert
         var doc = XDocument.Parse(xml);
         return JsonConvert.SerializeXNode(doc, Formatting.None, omitRootObject: true);
     }
-
-    private static JsonSerializer Instance { get; } = new JsonSerializer()
-    {
-        ContractResolver = GenericJsonPropertyNameContractResolver.Instance,
-        Converters =
-            {
-                BoolConverter.Instance,
-                TimestampToDateTimeConverter.Instance
-            },
-    };
 }
--- src/Flickr.Net.Core/Internals/JsonConverters/BoolConverter.cs
+++ src/Flickr.Net.Core/Internals/JsonConverters/BoolConverter.cs
@@ -2,28 +2,32 @@
 
 /// <summary>
 /// </summary>
-public class BoolConverter : JsonConverter
+public class BoolConverter : System.Text.Json.Serialization.JsonConverter<bool>
 {
+    public BoolConverter() : base() { }
+
     public static BoolConverter Instance { get; } = new();
-
-    /// <summary>
-    /// </summary>
-    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-    {
-        writer.WriteValue(((bool)value) ? 1 : 0);
-    }
-
-    /// <summary>
-    /// </summary>
-    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-    {
-        return reader.Value.ToString() == "1";
-    }
 
     /// <summary>
     /// </summary>
     public override bool CanConvert(Type objectType)
     {
         return objectType == typeof(bool);
+    }
+
+
+    /// <summary>
+    /// </summary>
+    public override bool Read(ref System.Text.Json.Utf8JsonReader reader, Type typeToConvert, System.Text.Json.JsonSerializerOptions options)
+    {
+        return reader.ValueTextEquals("1");
+    }
+
+
+    /// <summary>
+    /// </summary>
+    public override void Write(System.Text.Json.Utf8JsonWriter writer, bool value, System.Text.Json.JsonSerializerOptions options)
+    {
+        writer.WriteNumberValue(value ? 1 : 0);
     }
 }
