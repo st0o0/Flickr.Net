c4b8a235fab54f25192b20431d66cd14c2abca9e
Jan <64534642+st0o0@users.noreply.github.com>
2023-08-26 10:16:35 +0000

    little fix and improvements

--- src/Flickr.Net.Core/GlobalUsings.cs
+++ src/Flickr.Net.Core/GlobalUsings.cs
@@ -3,5 +3,3 @@ global using System.Runtime.Serialization;
 global using Flickr.Net.Core.Entities;
 global using Flickr.Net.Core.Enums;
 global using Flickr.Net.Core.Internals;
-global using Newtonsoft.Json;
-global using Newtonsoft.Json.Converters;
