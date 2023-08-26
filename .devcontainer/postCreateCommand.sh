9b84f6d081266abe6b102b0039c9d3d9bf80f220
Jan <64534642+st0o0@users.noreply.github.com>
2023-08-26 09:58:08 +0000

    it's working again 💌

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -10,4 +10,4 @@ wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
 
 chmod +x ./dotnet-install.sh
 
-./dotnet-install.sh --channel 8.0
+./dotnet-install.sh --channel 8.0 --install-dir /usr/local/dotnet/current
